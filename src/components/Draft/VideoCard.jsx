f36f0f7ffc2c2f8d27319531750fc69b651013d0
kesolink <kingsleyumune14@gmail.com>
2025-12-30 15:34:37 +0100

    improve draft editor and some page dark mode

--- src/components/Draft/VideoCard.jsx
+++ src/components/Draft/VideoCard.jsx
@@ -60,16 +60,7 @@ const VideoCard = ({  video, onEdit, onView, onDelete, onPublish}) => {
               </button> */}
             </>
           ) : (
-            <button 
-              className="btn btn--primary btn--sm animate-pulse" 
-              onClick={() => onPublish(video.id)}
-            >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14" />
-                <path d="M12 5l7 7-7 7" />
-              </svg>
-              Publish
-            </button>
+            ""
           )}
         </div>
       </div>
